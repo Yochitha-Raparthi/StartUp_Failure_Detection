94da6a92a6e73ad2567f56f69c878c145b1d1a4f
Yochitha-Raparthi <153537367+Yochitha-Raparthi@users.noreply.github.com>
2026-07-31 23:16:12 +0530

    small changes

--- client/src/context/ProjectContext.jsx
+++ client/src/context/ProjectContext.jsx
@@ -1,22 +1,4 @@
-// import { createContext, useState } from "react";
 
-// export const ProjectContext = createContext();
-
-// export function ProjectProvider({ children }) {
-
-//     const [project, setProject] = useState(null);
-
-//     return (
-//         <ProjectContext.Provider
-//             value={{ project, setProject }}
-//         >
-//             {children}
-//         </ProjectContext.Provider>
-//     );
-// }
-
-
-//new code
 import { createContext, useState } from "react";
 
 export const ProjectContext = createContext();
