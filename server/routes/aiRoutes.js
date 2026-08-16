c989214914c235057e4d653897b49d30a0af21d8
Yochitha-Raparthi <153537367+Yochitha-Raparthi@users.noreply.github.com>
2026-08-16 14:57:09 +0530

    add one route

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -2,9 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const { generateAnalysis } = require("../controllers/aiController");
-
+const {
+    generateRecommendations
+} = require("../controllers/recommendationController");
 router.post("/analyze", generateAnalysis);
-
+router.post(
+    "/recommend",
+    generateRecommendations
+);
 module.exports = router;
+
+
+
+
 
 
